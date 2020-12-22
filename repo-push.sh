c162a632cfb07905b6b62af302ca66efe6bc82c4
Gary Morse <gmorse81@gmail.com>
2020-12-22 18:48:55 -0500

    remove save flag

--- repo-push.sh
+++ repo-push.sh
@@ -2,9 +2,9 @@
 helm repo add ${PLUGIN_REPO_NAME} ${HELM_REPO_URL}
 helm repo add stable https://charts.helm.sh/stable
 if [ "${PLUGIN_UPDATE_DEPENDENCIES}" = "false" ]; then
-  helm package --version=$(cat .tags) --app-version=$(cat .tags) --save=false ${PLUGIN_CHART_SUBDIR}/.
+  helm package --version=$(cat .tags) --app-version=$(cat .tags) ${PLUGIN_CHART_SUBDIR}/.
 else
-  helm package -u --version=$(cat .tags) --app-version=$(cat .tags) --save=false ${PLUGIN_CHART_SUBDIR}/.
+  helm package -u --version=$(cat .tags) --app-version=$(cat .tags) ${PLUGIN_CHART_SUBDIR}/.
 fi
 RESPONSE=$(curl --data-binary "@${PLUGIN_CHART_SUBDIR}-$(cat .tags).tgz" ${HELM_REPO_URL}/api/charts)
 echo $RESPONSE
